22d3c286b9152d586e9b059e4bef2f48fdaeef98
Chandan <discoverchan9@gmail.com>
2023-03-06 08:30:24 -0400

    bug fix

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ cur.executescript('''
 count = input('How many urls would you like to search today? ')
 times = int(count)
 while times > 0 :
-    starturl = input('Enter a chess url you want to parse: ')
+    starturl = input('Enter a url you want to parse: ')
     web = urlformat(starturl)
     if (len(web) > 1) :
         cur.execute('INSERT OR IGNORE INTO masterurls (url) VALUES (?)',(web,))
@@ -35,28 +35,29 @@ while times > 0 :
             print('Error retrieving this url: ', document.getcode())
             cur.execute('UPDATE masterurls SET error = ? WHERE url = ?',(document.getcode(), web))
             con.commit()
+        cur.execute('UPDATE masterurls SET html = ? WHERE url = ?', (memoryview(html), web))
+        con.commit()
+
+        soup = BeautifulSoup(html, 'html.parser')
+        tags = soup('a')
+        count = 0
+        for tag in tags:
+            href = tag.get('href', None)
+            if href is None: continue
+            uparse = urllib.parse.urlparse(href)
+            if (len(uparse.scheme)) < 1:
+                href = urllib.parse.urljoin(web, href)
+            pos = href.rfind('#')
+            if (pos > 1): href = href[:pos]
+
+            cur.execute('INSERT OR IGNORE INTO extractedurls (url) VALUES (?)', (href,))
+            cur.execute('UPDATE extractedurls SET extractedfrom = ? WHERE url = ?', (web, href))
+            con.commit()
     except:
         print('Unable to retrieve or parse page, incorrect url format:', web)
         cur.execute('UPDATE masterurls SET error = -1 WHERE url = ?', (web,))
         con.commit()
-    cur.execute('UPDATE masterurls SET html = ? WHERE url = ?', (memoryview(html), web))
-    con.commit()
 
-    soup = BeautifulSoup(html, 'html.parser')
-    tags = soup('a')
-    count = 0
-    for tag in tags :
-        href = tag.get('href', None)
-        if href is None : continue
-        uparse = urllib.parse.urlparse(href)
-        if (len(uparse.scheme)) < 1 :
-            href = urllib.parse.urljoin(web, href)
-        pos = href.rfind('#')
-        if (pos > 1) : href = href[:pos]
-
-        cur.execute('INSERT OR IGNORE INTO extractedurls (url) VALUES (?)',(href,))
-        cur.execute('UPDATE extractedurls SET extractedfrom = ? WHERE url = ?',(web, href))
-        con.commit()
     times = times - 1
 
 cur.execute('SELECT url FROM masterurls')
